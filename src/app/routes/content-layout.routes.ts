66a821e934d887382b2cb41b1344357f917c601f
davidebalice <davide.balice@gmail.com>
2024-07-15 15:04:40 +0200

    update

--- src/app/routes/content-layout.routes.ts
+++ src/app/routes/content-layout.routes.ts
@@ -5,9 +5,5 @@ export const CONTENT_ROUTES: Routes = [
     path: 'auth',
     loadChildren: () => import('../auth/auth.module').then((m) => m.AuthModule),
   },
-  {
-    path: 'pages',
-    loadChildren: () =>
-      import('../pages/pages.module').then((m) => m.PagesModule),
-  },
+  
 ];
--- src/app/routes/full-layout.routes.ts
+++ src/app/routes/full-layout.routes.ts
@@ -6,44 +6,19 @@ export const Full_ROUTES: Routes = [
     loadChildren: () =>
       import('../dashboard/dashboard.module').then((m) => m.DashboardModule),
   },
-
   {
     path: 'products',
     loadChildren: () =>
       import('../products/products.module').then((m) => m.ProductsModule),
   },
-
   {
     path: 'components',
     loadChildren: () =>
       import('../components/components.module').then((m) => m.ComponentsModule),
   },
-
-  {
-    path: 'tables',
-    loadChildren: () =>
-      import('../tables/tables.module').then((m) => m.TablesModule),
-  },
-
-  {
-    path: 'widgets',
-    loadChildren: () =>
-      import('../widgets/widgets.module').then((m) => m.WidgetsModule),
-  },
-
-  {
-    path: 'faq',
-    loadChildren: () => import('../faq/faq.module').then((m) => m.FaqModule),
-  },
   {
     path: 'profile',
     loadChildren: () =>
       import('../profile/profile.module').then((m) => m.ProfileModule),
-  },
-
-  {
-    path: 'downloads',
-    loadChildren: () =>
-      import('../downloads/downloads.module').then((m) => m.DownloadsModule),
   },
 ];
